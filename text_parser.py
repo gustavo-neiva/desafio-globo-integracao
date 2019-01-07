f4fbc0cb5bc2dd560ec03dfe4f1c286230b2bdb2
Luiz Neiva <luizgzn@gmail.com>
2019-01-07 12:45:24 -0200

    Create method to parse text following the text pattern

--- text_parser.py
+++ text_parser.py
@@ -10,3 +10,33 @@ class TextParser:
         with open(file_path, "r", encoding="utf-8", errors='ignore') as f:
             n_lines = sum(bl.count("\n") for bl in self.__blocks(f))
             return n_lines
+
+    def parse_content(self, file_path, start_line = 0):
+        with open(file_path, 'r', encoding="utf-8", errors='ignore') as f:
+            result_list = []
+            # definir a linha onde iniciar o parseamento da string
+            for i, line in enumerate(f):
+                if i == start_line:
+                    break
+            for line in f:
+                if line.find('----') == 1:
+                    break
+            for line in f:
+                # selecionar pedaços da string a partir do indice
+                start_time = line[6:28]
+                end_time = line[29:50]
+                title = line[106:138].rstrip()
+                duration = line[184:195]
+                reconcile_key = line[279:298].rstrip()
+                dictionary = { 'start_time': start_time, 
+                            'end_time': end_time, 
+                            'title': title,
+                            'duration':duration,
+                            'reconcile_key': reconcile_key
+                            }
+                duration_string = duration.split(';', 1)[0]
+                dt = time.strptime(duration_string,'%H:%M:%S')
+                duration_seconds = datetime.timedelta(hours=dt.tm_hour,minutes=dt.tm_min,seconds=dt.tm_sec).total_seconds()
+                if duration_seconds > 30:
+                    result_list.append(dictionary)
+            return(result_list)
