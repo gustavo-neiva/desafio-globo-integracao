ee70ae8ba8482c5f10b1c13ce632a9445167481e
Luiz Neiva <luizgzn@gmail.com>
2019-01-07 11:50:51 -0200

    Create text line counter methods

--- text_parser.py
+++ text_parser.py
@@ -1,4 +1,12 @@
 
 class TextParser:
-    pass
-    
+    def __blocks(self, files, size=65536):
+        while True:
+            b = files.read(size)
+            if not b: break
+            yield b
+
+    def count_lines(self, file_path):
+        with open(file_path, "r", encoding="utf-8", errors='ignore') as f:
+            n_lines = sum(bl.count("\n") for bl in self.__blocks(f))
+            return n_lines
