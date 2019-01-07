0fa37aaa6ec514f2e6a16f884ecaa6cde1eb08e0
Luiz Neiva <luizgzn@gmail.com>
2019-01-07 15:09:35 -0200

    Add on_created event method

--- dir_event_handler.py
+++ dir_event_handler.py
@@ -1,4 +1,8 @@
 from watchdog.events import FileSystemEventHandler
 
 class DirEventHandler(FileSystemEventHandler):
-    pass
+    def process(self, event):
+        pass
+        
+    def on_created(self, event): # quando o arquivo for criado no diretorio
+        self.process(event)
