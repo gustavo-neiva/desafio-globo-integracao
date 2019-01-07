d2d67cb16131144bd6a33071a488b3c70e79357e
Luiz Neiva <luizgzn@gmail.com>
2019-01-07 15:24:09 -0200

    Connect the observer with the event handler and the text parser following the project business logic

--- dir_event_handler.py
+++ dir_event_handler.py
@@ -5,8 +5,23 @@ class DirEventHandler(FileSystemEventHandler):
         self.tp = tp
         self.path = path
 
-    def process(self, event):
-        pass
+    def process(self, event):      
+        # o conteudo informativo começa a partir da linha 8
+        # como cada novo arquivo contem as mesmas linhas do arquvio anterior mais as novas informaçoes
+        # a linha pelo qual o parser deve iniciar é igual ao número de linhas do arquivo
+        # anterior - 1 para deixar a contagem certa
+
+        # remover arquivos ocultos da observacao
+        list_dir = [f for f in os.listdir(self.path) if not f.startswith('.')]
+        if len(list_dir) > 1:
+            forehand_file_path = f'{self.path}/{sorted(list_dir)[-2]}'
+            forehand_file_lines = self.tp.count_lines(forehand_file_path)
+            start_line = forehand_file_lines - 1
+        else:
+            start_line = 7
+        print(start_line)
+        parsed_content = self.tp.parse_content(event.src_path, start_line)
+        print(parsed_content)
         
     def on_created(self, event): # quando o arquivo for criado no diretorio
         self.process(event)
