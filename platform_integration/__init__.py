1eb1e2dc7af829a51a96437e54d14c696ab494d7
Luiz Neiva <luizgzn@gmail.com>
2019-01-10 17:58:12 -0200

    Code refactor integrating classes

--- platform_integration/__init__.py
+++ platform_integration/__init__.py
@@ -12,8 +12,9 @@ path = sys.argv[1] if len(sys.argv) > 1 else '.'
 # seta o observer para usar o gerenciador de eventos no diretorio
 observer = Observer() # instancia uma classe do observador do watchdog
 tp = TextParser() # instancia a classe do parser
-cut_url = 'https://jsonplaceholder.typicode.com/photos' # api de exemplo para testes
+cut_url = 'https://jsonplaceholder.typicode.com/posts' # api de exemplo para testes
 globo_play_url = 'https://jsonplaceholder.typicode.com/posts'
-c = Client(cut_url, globo_play_url) # instancia a class do http client
+video_path = "path/to/save/video"
+client = Client(cut_url, globo_play_url, video_path) # instancia a class do http client
 # cria o gerenciador de eventos
-event_handler = DirEventHandler(tp, path, c)
+event_handler = DirEventHandler(tp, path, client)
