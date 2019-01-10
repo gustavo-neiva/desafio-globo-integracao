65d0b1682af57bc8611d894d3cb3708a818b620b
Luiz Neiva <luizgzn@gmail.com>
2019-01-10 17:57:41 -0200

    Add async logger to log requests and responses

--- platform_integration/client.py
+++ platform_integration/client.py
@@ -1,27 +1,34 @@
 import aiohttp
+import time
+import logging
+from aiologger import Logger
 
-class Client():
-    def __init__(self, cut_url, globo_play_url):
+logger = Logger.with_default_handlers(name='my-logger')
+
+class Client:
+    def __init__(self, cut_url, globo_play_url, video_path):
         self.cut_url = cut_url
         self.globo_play_url = globo_play_url
-        self.
+        self.video_path = video_path
 
-    async def post_cut(self, client, content):
-        async with client.post(self.cut_url) as resp:
-            assert resp.status == 200
+    async def post_cut(self, client, content, path):
+        to_cut = { "video_path": path,
+                      "to_cut": content}
+        async with client.post(self.cut_url, data=to_cut) as resp:
+            await logger.info(f'Request: {to_cut} - Response: {resp.json()}')
             return await resp.json()
 
-    async def get_cut(self, client):
-        async with client.get(self.cut_url) as resp:
-            assert resp.status == 200
-            return await resp.json()
+    async def get_cut(self, client, id):
+        async with client.get(f'{self.cut_url}/{id}') as resp:
+            return await resp.content.read(chunk_size)
 
     async def post_globo_play(self, client):
         async with client.post(self.globo_play_url) as resp:
-            assert resp.status == 200
             return await resp.json()
 
-    async def main(self, content):
+    async def request(self, content):
         async with aiohttp.ClientSession() as client:
-            html = await self.get_cut(client)
-            print(html[1])
+            post_cut = await self.post_cut(client, content, self.video_path)
+            # cut_id = post_cut.json(id)
+            # get_cut = await self.get_cut(client, cut_id)
+            return(post_cut)
