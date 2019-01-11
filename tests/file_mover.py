e3d8f2a94655348eb169103a483d5a5eb64f356f
Luiz Neiva <luizgzn@gmail.com>
2019-01-11 18:14:06 -0200

    Start a test suite

---
+++ tests/file_mover.py
@@ -0,0 +1,34 @@
+from os import makedirs, listdir, path, getcwd
+from os.path import isfile, join
+import shutil
+import sys
+import time
+
+dir_raw = path.realpath(__file__).split('/')
+dir_raw.pop()
+base_dir = "/".join(dir_raw)
+print(base_dir)
+if not path.exists(f'{base_dir}/folder_test'):
+    makedirs(f'{base_dir}/folder_test')
+
+folder_1 = sys.argv[1] if len(sys.argv) > 1 else f'{base_dir}/folder_full'
+folder_2 = sys.argv[2] if len(sys.argv) > 1 else f'{base_dir}/folder_test'
+
+def main():
+    files = [f for f in listdir(folder_1) if isfile(join(folder_1, f))]
+    for f in files:
+        print(f'Moving {folder_1}/{f}')
+        shutil.copy(f'{folder_1}/{f}', f'{folder_2}/{f}')
+        time.sleep(0.5)
+    
+def remove_files():
+    shutil.rmtree(folder_2)
+    makedirs(folder_2)
+
+if __name__ == '__main__':
+    while True:
+        user_input = input("Pressione ENTER para iniciar o teste, ESPAÇO + ENTER para resetar e CTRL + C para parar")
+        if user_input == "":
+            main()
+        if user_input == " ":
+            remove_files()
